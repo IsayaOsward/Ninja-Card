efc4c3c4ff3f07e9420a33853acee93ffebff9c4
IsayaOsward <ionthefirst97@gmail.com>
2023-09-04 10:23:10 +0300

    A Simple Ninja Card

---
+++ ninja_license_id/android/app/src/main/kotlin/com/example/ninja_license_id/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ninja_license_id
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ninja_license_id/lib/main.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: NinjaCard(),
+  ));
+}
+class NinjaCard extends StatelessWidget {
+  const NinjaCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        title: Text("Ninja Card"),
+        centerTitle: true,
+        backgroundColor: Colors.grey[850],
+        elevation: 0,
+      ),
+      body: Padding(
+        padding: EdgeInsets.fromLTRB(30, 40, 30, 0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start   ,
+          children: [
+            Center(
+              child: CircleAvatar(
+                backgroundImage: NetworkImage("https://unsplash.com/photos/Os6oT6rGg40", scale: 100),
+                radius: 60,
+              ),
+            ),
+            Divider(
+              height: 60,
+              color: Colors.grey[800],
+            ),
+            Text(
+              'NAME',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              'Ion The First™ ',
+              style: TextStyle(
+                color: Colors.yellow[200],
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 30,),
+            Text(
+              'CURRENT NINJA LEVEL',
+              style: TextStyle(
+                color: Colors.grey,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height: 10,),
+            Text(
+              '10  ',
+              style: TextStyle(
+                color: Colors.yellow[200],
+                fontSize: 28,
+                fontWeight: FontWeight.bold,
+                letterSpacing: 2,
+              ),
+            ),
+            SizedBox(height:30),
+            Row(
+              children: [
+                Icon(
+                  Icons.email,
+                  color: Colors.grey[200],
+                ),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(5, 0, 0, 0),
+                  child: Text(
+
+                    'ionthefirst97@gmail.com',
+                    style: TextStyle(
+                      color: Colors.yellow[200],
+                      fontSize: 28,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
